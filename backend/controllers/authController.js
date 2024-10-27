81b4b1cdb88063e228bf592003b5624243739477
naomi gong <naomigong2005@gmail.com>
2024-10-27 16:02:06 -0700

    small json string fixes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,7 +9,7 @@ const signup = async (req, res) =>{
         const user = await UserModel.findOne({email});
         if(user){
             return res.status(409)
-            .json({message: 'User is already existm you can login', success: false})
+            .json({message: 'User is already exist you can login', success: false})
         }
         const userModel = new UserModel({firstname, lastname, email, password})
         userModel.password = await bcrypt.hash(password, 10)
@@ -49,7 +49,7 @@ const login = async (req, res) =>{
         )
         res.status(201)
             .json({
-                message : "Signup successful",
+                message : "Login successful",
                 success: true,
                 jwtToken,
                 email, 
--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,3 +1,4 @@
+
 import { Router } from 'express';
 import {ensureAuthenticated} from '../middleware/auth.js'
 
