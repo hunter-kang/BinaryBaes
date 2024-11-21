716b2017166242cfc091ca0821af183621f4903c
Simon Koski <simon_koski@icloud.com>
2024-11-21 05:07:31 -0800

    Updated profile a lot

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -3,39 +3,70 @@ import "../styles/Text.css";
 import "../styles/Profile.css";
 import TaskBar from '../components/TaskBar';
 import logo from '../assets/logo-design.png';
+import snoopy from '../assets/snoopy_sample_img.png'
+import snoopy2 from '../assets/snoopy.jpeg'
 
 export default function Profile(){
     return(
         <div className="profile-container">
-            <img className="profile-photo" src={logo} alt="hi"/>
-            <p className="name">
-                Carin Dang
+            <p className="profile-text">
+                #Profile Information
             </p>
-            <p className="pronouns">she/her</p>
-            <img className="photo" src={logo} alt="hi"></img>
-            <div className="info-container">
-                <div className="top-row">
-                    <p className="info-text">19</p>
-                    <p className="info-text">Nonbinary</p>
-                    <p className="info-text">Straight</p>
+            <div className="row">
+                <div className="info-container">
+                    <p className="question-text">Snoopy = Profile():</p>
+                    <p className="info-text"> • Orientation: Binary Orientation: Binary Orientation: Binary Orientation: Binary </p>
+                    <p className="info-text"> • Gender: Non-binary Gender: Non-binary Gender: Non-binary Gender: Non-binary </p>
                 </div>
-                <p className="info-text">Temple City</p>
-                <p className="info-text">Long-term</p>
-                <p className="info-text">Monogamy</p>
-                <p className="info-text">UCLA</p>
+                <div className="answer-container">
+                </div>
             </div>
-            <img className="photo" src={logo} alt="hi"></img>
-            <div className = "question-container">
-                <p className="question-text">If you could be anyone for a day who would you be?</p>
-                <p className="question-answer">Myself lol!</p>
+            <p className="prompt-text">#Prompt 1</p>
+            <div className="row">
+                <div className="info-container">
+                    <p className="question-text">def your_favorite_color():</p>
+                    <p className="info-text">mint green mint green mint green mint green mint green mint green mint green</p>
+                    <p className="info-text">sky blue sky blue sky blue sky blue sky blue sky blue sky blue sky blue </p>
+                </div>
+                <div className="img-container">
+                    <img className="photo" src={snoopy} alt="hi"></img>
+                    <p className="png-text">.png</p>
+                </div>
             </div>
-            <img className="photo" src={logo} alt="hi"></img>
-            <div className = "question-container">
-                <p className="question-text">If you could be anyone for a day who would you be?</p>
-                <p className="question-answer">Myself lol!</p>
+            <p className="prompt-text">#Prompt 2</p>
+            <div className="row">
+                <div className="info-container">
+                    <p className="question-text">def your_favorite_color():</p>
+                </div>
+                <div className="info-container">
+                    <img className="photo" src={snoopy2} alt="hi"></img>
+                    <p className="png-text">.png</p>
+                </div>
             </div>
-            <TaskBar page = "PROFLIE"/>
+            <p className="prompt-text">#Prompt 3</p>
+            <div className="question-container">
+                <p className="question-text">def your_favorite_color(): def your_favorite_color(): def your_favorite_color():</p>
+                <div className="row">
+                    <div className="answer-container">
+                        <p className="info-text">mint green mint green mint green mint green mint green mint green mint green</p>
+                        <p className="info-text">sky blue sky blue sky blue sky blue sky blue sky blue sky blue sky blue </p>
+                    </div>
+                    <div className="answer-container">
+                        <img className="photo" src={snoopy2} alt="hi"></img>
+                        <p className="png-text">.png</p>
+                    </div>
+                </div>
+            </div>
+            <p className="prompt-text">#Prompt 4</p>
+            <div className="row">
+                <div className="answer-container">
+                    <img className="photo" src={snoopy2} alt="hi"></img>
+                    <p className="png-text">.png</p>
+                </div>
+                <div className="answer-container">
+                </div>
+            </div>
+            {/*<TaskBar page = "PROFLIE"/>*/}
         </div>
-        
     );
 }
